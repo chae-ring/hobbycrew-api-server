e1ab8cca4c45ce62bc9613cb1cbdd49b8d88d772
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-03-29 20:10:42 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,15 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
+  const origins = ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: origins,
+    credentials: true,
+    methods: ['POST', 'PUT', 'GET', 'PATCH', 'DELETE'],
+    optionsSuccessStatus: 200,
+  });
+
   await app.listen(3000, '0.0.0.0'); //포트 3000에서 HTTP 요청을 수신하도록 설정
 }
 bootstrap();
