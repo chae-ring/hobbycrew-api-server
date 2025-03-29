6aa07d6220e5aaa5b8e3b31f1aaa04f54405cd1f
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-03-29 14:06:56 +0900

    post module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     PrismaModule,
     AuthModule,
+    PostModule,
   ], //configModule 전역 설정
   controllers: [AppController],
   providers: [AppService],
